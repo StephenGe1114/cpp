c191dd3d04786ad6624b063fd9a6c4bcb6ae05c3
gsw <1499186594@qq.com>
2022-05-26 23:05:01 +0800

    learn cpp

---
+++ container/vector.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 14991 on 2022/5/26.
+//
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> a{1, 2, 3, 4};
+    return 0;
+}
+
